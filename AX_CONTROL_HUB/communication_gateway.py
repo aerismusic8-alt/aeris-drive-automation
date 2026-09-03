3b91345d330bf8107e2095aa08c3d7a278b2e4d8
aerismusic8-alt <aerismusic8@gmail.com>
2026-09-03 10:31:59 +0700

    fix: default gateway state version without mutating master state

--- AX_CONTROL_HUB/communication_gateway.py
+++ AX_CONTROL_HUB/communication_gateway.py
@@ -79,7 +79,7 @@ class CommunicationGateway:
             'identity_under_test': 'A',
             'authority': state.get('authority'),
             'master_status': state.get('status'),
-            'state_version': state.get('state_version'),
+            'state_version': int(state.get('state_version', 0)),
             'rehydration_status': challenge['status'],
             'model_independence': state.get('model_independence') is True,
             'runtime_profile': challenge.get('runtime_profile'),
