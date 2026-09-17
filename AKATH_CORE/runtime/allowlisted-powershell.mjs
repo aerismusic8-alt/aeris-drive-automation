05d8737e989c251af14c4b4e3b0a961fddbe070d
aerismusic8-alt <aerismusic8@gmail.com>
2026-09-17 20:12:23 +0700

    feat: add safe autonomous PowerShell actions

--- AKATH_CORE/runtime/allowlisted-powershell.mjs
+++ AKATH_CORE/runtime/allowlisted-powershell.mjs
@@ -6,6 +6,12 @@ const ACTIONS = Object.freeze({
   }),
   runtime_identity: Object.freeze({
     script: '$PSVersionTable.PSVersion.ToString()'
+  }),
+  runtime_process_snapshot: Object.freeze({
+    script: 'Get-Process | Where-Object { $_.ProcessName -match "node|powershell" } | Select-Object -First 20 Id,ProcessName | Format-Table -AutoSize | Out-String'
+  }),
+  runtime_disk_snapshot: Object.freeze({
+    script: 'Get-PSDrive -PSProvider FileSystem | Select-Object Name,Free,Used | Format-Table -AutoSize | Out-String'
   })
 });
 
